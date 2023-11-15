25385af9a30d3228e128bb4b9d1113497381a2cb
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-15 20:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ function isSameType(value1, value2) {
     }
 
     // Check if the types are the same
-    return typeof value1 === typeof value2;
+    if (typeof value1 !== typeof value2) {
+        return false;
+    }
+
+    // Check if both values are strings
+    if (typeof value1 === 'string' && typeof value2 === 'string') {
+        // Use strict equality to compare strings
+        return value1 === value2;
+    }
+
+    // For other types, return true
+    return true;
 }
 
-// Example usage with user input
-let userInput1 = prompt("Enter Start of the Range.");
-let userInput2 = prompt("Enter End Of the Range.");
+// Example usage without user input
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
 
 // Convert the user inputs to the appropriate data types
-let value1 = parseFloat(userInput1); // Assuming these are numbers
-let value2 = parseFloat(userInput2);
+// Assuming these are numbers
+value1 = parseFloat(value1);
+value2 = parseFloat(value2);
 
 // Check and alert the result
 alert(isSameType(value1, value2));
